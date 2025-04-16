336936c19d454782df4a7d6df5b2c5c6205992fc
khatitaha <khatitahasocial@gmail.com>
2025-04-16 23:37:57 +0100

    Add placeholder text for model development in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
       <main className="flex-1 flex flex-col gap-6 px-4  ">
               <Hero />
-
+im making the model so 
       </main>
   );
 }
