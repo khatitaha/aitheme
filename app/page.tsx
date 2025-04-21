6b72c11e52f48e6b800400a64e3b54d0f8594ace
khatitaha <khatitahasocial@gmail.com>
2025-04-21 23:16:21 +0100

    training

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,10 @@ import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Home() {
   return (
-      <main className="flex-1 flex flex-col gap-6 px-4  ">
-              <Hero />
-im making the model so 
-      </main>
+    <main className="flex-1 flex flex-col gap-6 px-4  ">
+      <Hero />
+      im making the model so
+      im training the model
+    </main>
   );
 }
